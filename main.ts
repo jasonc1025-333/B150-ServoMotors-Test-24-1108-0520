7f2e4b7a435fbb8f27d9bb703b02837dc582c033
jasonc1025-333 <67285598+jasonc1025-333@users.noreply.github.com>
2025-02-10 09:04:12 -0800

    25-0210-0900 *** jasonc1025-333/23-1006-1820-rq100-questcustomblocks-wukong-oled:v0.14.2

--- main.ts
+++ main.ts
@@ -11,7 +11,7 @@ input.onButtonPressed(Button.A, function () {
     50,
     0
     )
-    quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(0, quest_Time_Units_Enum.Seconds)
+    quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(3, quest_Time_Units_Enum.Seconds)
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -38,7 +38,7 @@ input.onButtonPressed(Button.AB, function () {
     50,
     50
     )
-    quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(0, quest_Time_Units_Enum.Seconds)
+    quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(3, quest_Time_Units_Enum.Seconds)
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -65,7 +65,7 @@ input.onButtonPressed(Button.B, function () {
     0,
     50
     )
-    quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(0, quest_Time_Units_Enum.Seconds)
+    quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(3, quest_Time_Units_Enum.Seconds)
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -79,6 +79,112 @@ input.onButtonPressed(Button.B, function () {
     0
     )
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (true) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        50,
+        50,
+        quest_Turn_Duration_Enum.msec_3000,
+        quest_Debug_Show_Enum.Off
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(3, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        -50,
+        -50,
+        quest_Turn_Duration_Enum.msec_3000,
+        quest_Debug_Show_Enum.Off
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(3, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        50,
+        -50,
+        quest_Turn_Duration_Enum.msec_3000,
+        quest_Debug_Show_Enum.Off
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(3, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        -50,
+        50,
+        quest_Turn_Duration_Enum.msec_3000,
+        quest_Debug_Show_Enum.Off
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(3, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_3000,
+        quest_Debug_Show_Enum.Off
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(3, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            # # . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_3000,
+        quest_Debug_Show_Enum.Off
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(3, quest_Time_Units_Enum.Seconds)
+    }
+})
 quest_Note_3.quest_Show_String_For_Note_Big_Func(
 "#master"
 )
@@ -88,3 +194,8 @@ quest_Note_1.quest_Show_String_For_Note_Small_Func(
 basic.showIcon(IconNames.Heart)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
 basic.showIcon(IconNames.Happy)
+quest_Dashboard.quest_Send_LoginOfBot_ToXrayDashboardOfMb_Func(
+0,
+quest_Toggle_OnOff_Enum.On,
+quest_Debug_Show_Enum.Dashboard_OLED
+)
